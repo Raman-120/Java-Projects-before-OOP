f9c6387f9b04d366682300dabf93616086c4c671
Raman-120 <ramanchaulagain059@gmail.com>
2026-02-15 19:01:21 +0545

    Added store marks feature

--- JavaProjects/src/studentManagementSystem.java
+++ JavaProjects/src/studentManagementSystem.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class studentManagementSystem {
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args){
         // a simple student management system in java
 
@@ -15,22 +16,43 @@ public class studentManagementSystem {
         7.Menu-driven system
          */
 
-        Scanner scanner = new Scanner(System.in);
+
 
         String name;
         int roll;
-        double marks;
+        double marks = 0;
         int userAnswer;
+        double[][] StudentMarks = new double[1][10];
 
 
         System.out.println("----Welcome to the Student Management System----");
 
-        System.out.println(" 1.Store Marks \n 2. Update Marks \n 3. Average marks \n 4. Calculate grade");
+        System.out.println("1.Store Marks \n 2. Update Marks \n 3. Average marks \n 4. Calculate grade");
 
         System.out.print("What would you like to do?(1-4): ");
         userAnswer = scanner.nextInt();
 
+        switch(userAnswer){
+            case 1 ->{
+                storeMarks(marks);
+            }
 
+
+            default -> System.out.println("Invalid input");
+        }
         scanner.close();
+    }
+
+    static double storeMarks(double mark){
+
+        int roll;
+        System.out.println("Enter the roll no of the student: ");
+        roll = scanner.nextInt();
+
+
+        System.out.println("Enter the marks of the student: ");
+        mark = scanner.nextDouble();
+        return mark;
+
     }
 }
